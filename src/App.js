ef11ed857a7cfa85d046114ce2574f0bd652fbd2
Bpgate <3714359-bpgate@users.noreply.gitlab.com>
2021-01-19 16:31:34 +0300

    favicon images and other

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hello from V2</h1>
+          <h1>Bpgate</h1><h2>go@bpgate.com</h2>
+
         </header>
       </div>
   );
